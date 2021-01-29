2d2a571c950e59e8918b324fc337dd1a72e66268
tseno <tseno26@gmail.com>
2021-01-29 23:48:23 +0900

    5日目完了

--- React/react-starter/src/App.js
+++ React/react-starter/src/App.js
@@ -1,25 +1,22 @@
-import React, { useState, useCallback, memo, useEffect } from "react";
+import React, { Component } from "react";
+import MyBook from "./MyBook";
 
-const Button = memo(({ id, onClick }) => {
-  useEffect(() => {
-    console.log(`render ${id}`);
-  });
-  return <button onClick={onClick}>{id}</button>;
-});
+class MyHello extends Component {
+  render() {
+    const style = { color: "red", backgroundColor: "yellow" };
+    console.log(this.props);
 
-function App() {
-  const [count1, setCount1] = useState(0);
-  const increment1 = useCallback(() => setCount1(count1 + 1), [count1]);
-  const [count2, setCount2] = useState(0);
-  const increment2 = useCallback(() => setCount2((count) => count + 1), []);
-  return (
-    <div>
-      <p>{count1}</p>
-      <Button id={"Increment1"} onClick={increment1} />
-      <p>{count2}</p>
-      <Button id={"Increment2"} onClick={increment2} />
-    </div>
-  );
+    return (
+      <div>
+        <MyBook isbn={this.props.info.isbn} title={this.props.info.title}></MyBook>
+        <ul>
+          <li>書名：{this.props.info.title}</li>
+          <li>価格：{this.props.info.price}</li>
+          <li>出版社：{this.props.info.published}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyHello;
--- React/react-starter/src/index.js
+++ React/react-starter/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import MyHello from "./App";
+
+const info = {
+  title: "今日は寒い",
+  isbn: "WGS-JST-001",
+  price: 1000,
+  published: "WING",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyHello info={info}>今日は寒い。</MyHello>
   </React.StrictMode>,
   document.getElementById("root")
 );
